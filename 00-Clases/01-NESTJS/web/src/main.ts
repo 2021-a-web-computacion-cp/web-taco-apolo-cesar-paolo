e077479cfb32bee419e2f6b6b9f4e307ad59dff6
Cesar Taco <cesar.taco@epn.edu.ec>
2021-08-31 10:20:41 -0500

    Typescript basic sintax
    arrays, conditionals, strings, etc

--- 00-Clases/01-NESTJS/web/src/main.ts
+++ 00-Clases/01-NESTJS/web/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
 }
 bootstrap();
 
+/*
 // package.json ;
 // npm run start -> levantar servidor ;
 // node js command prompt
@@ -46,8 +47,116 @@ const usuario: Usuario = new Usuario('Cesar', 'Taco');
 usuario.nombre;
 usuario.apellido;
 
-interface UuarioInterface {
+interface UsuarioInterface {
   nombre: string;
   apellido: string;
   edad?: number; // ? -> opcional // valor por defecto ((undefined))
 }
+
+const objetoUsuario: UsuarioInterface = { nombre: 'Cesar', apellido: 'Taco' };
+objetoUsuario.nombre;
+objetoUsuario.apellido;
+objetoUsuario.edad;
+console.log(usuario);
+console.log(objetoUsuario);
+
+// PUNTEROS REFERENCIAS
+// PRIMITIVAS
+
+let edadAntigua = 22;
+let otraEdad = edadAntigua; //VALOR
+edadAntigua += 1; //23
+otraEdad += 1; //21
+
+//OBJETO
+const objetoEdad = {
+  edad: 22,
+};
+
+const otraEdadObjeto = objetoEdad; // REFERENCIA
+otraEdadObjeto.edad = otraEdadObjeto.edad + 1; //23
+console.log(otraEdadObjeto.edad);
+objetoEdad.edad; //23
+console.log(otraEdadObjeto.edad + 1); //24
+otraEdadObjeto.edad; //24
+const otraEdadObejtoClonado = { ...objetoEdad }; //CLONACION DE OBJETOS
+
+//ARREGLOS
+const arregloTodo = [1, '', true, null, new Date()];
+const arregloNumeros: number[] = [1, 2, 3, 4, 5];
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function funcionConNombre() {}
+
+const indice = arregloNumeros.findIndex((numero) => {
+  // FUNCION ANONIMA POR QUE NO TIENE NOMBRE
+  const elValorEsIgualATres: boolean = numero === 3;
+  return elValorEsIgualATres; // CONDICION BOOLEANA
+});
+
+arregloNumeros[indice] = 6;
+//agregar al final
+arregloNumeros.push(6);
+//agregar al principio
+arregloNumeros.unshift(0);
+
+// CONDICIONES (TRUTY Y FALSY)
+const numero = 0;
+if (numero) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if (1) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if (-1) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+//STRINGS
+if ('') {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if ('a') {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if ({}) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if ({ a: 1 }) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+//ARREGLOS
+if ([]) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if ([1]) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if (null) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+if (undefined) {
+  console.log('Truty');
+} else {
+  console.log('Falsy');
+}
+*/
